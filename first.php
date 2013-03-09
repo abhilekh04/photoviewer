3124139eecded1489b1c9315d749c9e759d2069c
abhilekh04 <abhilekh04@yahoo.com>
2013-03-09 04:43:20 -0700

    Create first.php

---
+++ first.php
@@ -0,0 +1,23 @@
+
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+<title>php-sdk</title>
+<style>
+body {
+font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+}
+h1 a {
+text-decoration: none;
+color: #3b5998;
+}
+h1 a:hover {
+text-decoration: underline;
+}
+</style>
+</head>
+<body>
+<h1>php-sdk</h1>
+
+</body>
+</html>
